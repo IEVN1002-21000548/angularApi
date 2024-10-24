1f47f8ba75dd30638ef7823f52ad4846fcf62160
Ricardo Alexis Hernandez Rodriguez <21000548@alumnos.utleon.edu.mx>
2024-10-23 21:07:38 -0600

    angularApi

---
+++ src/app/tem/addmessage/addmessage.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-addmessage',
+  standalone: true,
+  imports: [],
+  templateUrl: './addmessage.component.html',
+  styles: ``
+})
+export class AddmessageComponent {
+
+  constructor(public messajeService : MessageserviceService) {}
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,js}', // Ajusta las rutas según donde estén tus archivos
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
